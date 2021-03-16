abffc10ce3e6b391ad133c8364e6d0b2a2d4693c
Vusal Ismayilov <woosal@protonmail.com>
2021-03-16 18:26:20 +0300

    1.0

---
+++ meme-generator/src/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import './App.css';
+
+function Header() {
+    return (
+        <div className="Header">
+            <header>
+                <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+                alt="HeHEHe"/>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
